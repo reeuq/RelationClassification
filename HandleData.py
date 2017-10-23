d1c93c094d09c95522feea434bc58adcbaf0bd96
wyd <wangyadao@gmail.com>
2017-10-23 18:10:07 +0800

    init

---
+++ HandleData.py
@@ -0,0 +1,24 @@
+from gensim.models import word2vec
+import xml.dom.minidom
+import os
+# import util
+import numpy as py
+
+model = word2vec.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+print(model['world'])
+
+
+
+
+
+# dom = xml.dom.minidom.parse('1.1.text.xml')
+# root = dom.documentElement
+#
+# tt = root.getElementsByTagName("doc")
+# t = tt[0]
+# print(t.childNodes[0].data)
+# ss = t.getElementsByTagName("entity")
+# s = ss[0]
+# print(s.firstChild.data)
+# # sentences = word2vec.LineSentence(u"/home/wyd/PycharmProjects/RelationClassification")  # 加载语料
+# # model = word2vec.Word2Vec(sentences, size=200)
