f3712a97fd447a324d0c1acea8c0b7b6e3533b93
reeuq <1250608467@qq.com>
2017-11-27 14:36:47 +0800

    update

--- evaluation/Evaluation.py
+++ evaluation/Evaluation.py
@@ -17,16 +17,16 @@ def precision_each_class(predictions, labels):
                                         & (np.argmax(labels, 1) == np.argmax(zeros, 1)))
                          / np.sum(np.argmax(predictions, 1) == np.argmax(zeros, 1)))
     final_result = {
-        "usage_reverse": ("%.2f" % categories[0]),
-        "usage": ("%.2f" % categories[1]),
-        "result_reverse": ("%.2f" % categories[2]),
-        "result": ("%.2f" % categories[3]),
-        "model_feature_reverse": ("%.2f" % categories[4]),
-        "model_feature": ("%.2f" % categories[5]),
-        "part_whole_reverse": ("%.2f" % categories[6]),
-        "part_whole": ("%.2f" % categories[7]),
-        "topic_reverse": ("%.2f" % categories[8]),
-        "topic": ("%.2f" % categories[9]),
+        "usage": ("%.2f" % categories[0]),
+        "usage_reverse": ("%.2f" % categories[1]),
+        "result": ("%.2f" % categories[2]),
+        "result_reverse": ("%.2f" % categories[3]),
+        "model_feature": ("%.2f" % categories[4]),
+        "model_feature_reverse": ("%.2f" % categories[5]),
+        "part_whole": ("%.2f" % categories[6]),
+        "part_whole_reverse": ("%.2f" % categories[7]),
+        "topic": ("%.2f" % categories[8]),
+        "topic_reverse": ("%.2f" % categories[9]),
         "compare": ("%.2f" % categories[10])
     }
     return final_result
@@ -41,16 +41,16 @@ def recall_each_class(predictions, labels):
                                         & (np.argmax(labels, 1) == np.argmax(zeros, 1)))
                          / np.sum(np.argmax(labels, 1) == np.argmax(zeros, 1)))
     final_result = {
-        "usage_reverse": ("%.2f" % categories[0]),
-        "usage": ("%.2f" % categories[1]),
-        "result_reverse": ("%.2f" % categories[2]),
-        "result": ("%.2f" % categories[3]),
-        "model_feature_reverse": ("%.2f" % categories[4]),
-        "model_feature": ("%.2f" % categories[5]),
-        "part_whole_reverse": ("%.2f" % categories[6]),
-        "part_whole": ("%.2f" % categories[7]),
-        "topic_reverse": ("%.2f" % categories[8]),
-        "topic": ("%.2f" % categories[9]),
+        "usage": ("%.2f" % categories[0]),
+        "usage_reverse": ("%.2f" % categories[1]),
+        "result": ("%.2f" % categories[2]),
+        "result_reverse": ("%.2f" % categories[3]),
+        "model_feature": ("%.2f" % categories[4]),
+        "model_feature_reverse": ("%.2f" % categories[5]),
+        "part_whole": ("%.2f" % categories[6]),
+        "part_whole_reverse": ("%.2f" % categories[7]),
+        "topic": ("%.2f" % categories[8]),
+        "topic_reverse": ("%.2f" % categories[9]),
         "compare": ("%.2f" % categories[10])
     }
     return final_result
@@ -69,16 +69,16 @@ def f1_each_class(predictions, labels):
                   / np.sum(np.argmax(labels, 1) == np.argmax(zeros, 1)))
         categories[a] = (2 * precision * recall) / (precision + recall)
     final_result = {
-        "usage_reverse": ("%.2f" % categories[0]),
-        "usage": ("%.2f" % categories[1]),
-        "result_reverse": ("%.2f" % categories[2]),
-        "result": ("%.2f" % categories[3]),
-        "model_feature_reverse": ("%.2f" % categories[4]),
-        "model_feature": ("%.2f" % categories[5]),
-        "part_whole_reverse": ("%.2f" % categories[6]),
-        "part_whole": ("%.2f" % categories[7]),
-        "topic_reverse": ("%.2f" % categories[8]),
-        "topic": ("%.2f" % categories[9]),
+        "usage": ("%.2f" % categories[0]),
+        "usage_reverse": ("%.2f" % categories[1]),
+        "result": ("%.2f" % categories[2]),
+        "result_reverse": ("%.2f" % categories[3]),
+        "model_feature": ("%.2f" % categories[4]),
+        "model_feature_reverse": ("%.2f" % categories[5]),
+        "part_whole": ("%.2f" % categories[6]),
+        "part_whole_reverse": ("%.2f" % categories[7]),
+        "topic": ("%.2f" % categories[8]),
+        "topic_reverse": ("%.2f" % categories[9]),
         "compare": ("%.2f" % categories[10])
     }
     return final_result
@@ -86,16 +86,16 @@ def f1_each_class(predictions, labels):
 
 def f1_each_class_precision_recall(precision, recall):
     final_result = {
-        "usage_reverse": -1,
         "usage": -1,
-        "result_reverse": -1,
+        "usage_reverse": -1,
         "result": -1,
-        "model_feature_reverse": -1,
+        "result_reverse": -1,
         "model_feature": -1,
-        "part_whole_reverse": -1,
+        "model_feature_reverse": -1,
         "part_whole": -1,
-        "topic_reverse": -1,
+        "part_whole_reverse": -1,
         "topic": -1,
+        "topic_reverse": -1,
         "compare": -1
     }
     for a in final_result.keys():
